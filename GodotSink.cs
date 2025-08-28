b754eef797896136719860d9892ae4c5ff29b939
永安404 <101850798+YongAn404@users.noreply.github.com>
2025-08-28 08:38:17 +0800

    添加项目文件。

---
+++ GodotSink.cs
@@ -0,0 +1,51 @@
+﻿using Godot;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Serilog.Sinks.Godot
+{
+    public class GodotSink(ITextFormatter formatter) : ILogEventSink
+    {
+        private readonly ITextFormatter formatter = formatter;
+
+        public void Emit(LogEvent logEvent)
+        {
+            using var buffer = new StringWriter();
+
+            formatter.Format(logEvent, buffer);
+
+            string message = buffer.ToString().Trim();
+
+            switch (logEvent.Level)
+            {
+                case LogEventLevel.Verbose:
+                    GD.PrintRich(message);
+                    break;
+                case LogEventLevel.Debug:
+                case LogEventLevel.Information:
+                    GD.Print(message);
+                    break;
+                case LogEventLevel.Warning:
+                    if (logEvent.Exception == null)
+                    {
+                        GD.PrintRich($"[color=GOLD]{message}[/color]");
+                        return;
+                    }
+                    GD.PushWarning($"{message}::{logEvent.Exception.Message}");
+                    break;
+                case LogEventLevel.Error:
+                case LogEventLevel.Fatal:
+                    if (logEvent.Exception == null)
+                    {
+                        GD.PrintErr(message);
+                        return;
+                    }
+                    GD.PushError($"{message}::{logEvent.Exception.Message}");
+                    break;
+            }
+        }
+    }
+}
---
+++ GodotSinkExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Godot
+{
+    public static class GodotSinkExtensions
+    {
+        private const string DefaultDebugOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+        public static LoggerConfiguration Godot(this LoggerSinkConfiguration sinkConfiguration, LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum, string outputTemplate = DefaultDebugOutputTemplate, IFormatProvider? formatProvider = null, LoggingLevelSwitch? levelSwitch = null)
+        {
+            ArgumentNullException.ThrowIfNull(sinkConfiguration);
+            ArgumentNullException.ThrowIfNull(outputTemplate);
+
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            return sinkConfiguration.Godot(formatter, restrictedToMinimumLevel, levelSwitch);
+        }
+
+        public static LoggerConfiguration Godot(this LoggerSinkConfiguration sinkConfiguration, ITextFormatter formatter, LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum, LoggingLevelSwitch? levelSwitch = null)
+        {
+            ArgumentNullException.ThrowIfNull(sinkConfiguration);
+            ArgumentNullException.ThrowIfNull(formatter);
+            return sinkConfiguration.Sink(new GodotSink(formatter), restrictedToMinimumLevel, levelSwitch);
+        }
+    }
+}
